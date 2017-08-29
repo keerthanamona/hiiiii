efba32e658e37664c5b3e4586dcf8735c39b7c47
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2017-08-29 16:06:45 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char c;
+int isuppercasevowel,islowercasevowel;
+if(islowercasevowel||isuppercasevowel)
+{
+printf("is a vowel");
+}
+else
+{
+printf("is a consonent");
+}
+}
