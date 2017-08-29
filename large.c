0955435217732f36fac6d5da3beb7ef343726c87
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2017-08-29 16:21:46 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+int largest;
+printf("enter different type of number");
+scanf("%d%d%d",&a&b&c);
+if((a>b&&a>c)
+largest=a;
+}
+else if(b>a&&b>c)
+{
+largest=b;
+}
+else
+{
+largest=c;
+}
+printf"the largest number is ",largest);
+}
